912d2213baf7c4cec02de78625ff55afbaf31576
Rehcra001 <tkropf12345@gmail.com>
2023-11-15 18:47:08 +0200

    UPDATE TO .NET 8.0

--- LearnUnitTesting/LeetCodeSolutionsLibrary/BinaryTree/BinaryTreeTraversal.cs
+++ LearnUnitTesting/LeetCodeSolutionsLibrary/BinaryTree/BinaryTreeTraversal.cs
@@ -52,16 +52,16 @@ namespace LeetCodeSolutionsLibrary.BinaryTree
             return preorder;
         }
 
-        public static void PreorderRecursion( TreeNode root, List<int> preorder)
+        public static void PreorderRecursion(TreeNode root, List<int> preorder)
         {
             if (root is null)
             {
                 return;
             }
 
-            preorder.Add(root.val);            
-            PreorderRecursion( root.left, preorder);
-            PreorderRecursion( root.right, preorder);
+            preorder.Add(root.val);
+            PreorderRecursion(root.left, preorder);
+            PreorderRecursion(root.right, preorder);
         }
 
         public static IList<int> InorderTraversalIterative(TreeNode root)
@@ -166,6 +166,217 @@ namespace LeetCodeSolutionsLibrary.BinaryTree
 
             //go right
             InorderRecurse(inorder, root.right);
+        }
+
+        public static IList<int> PostorderTraversalIterative(TreeNode root)
+        {
+            List<int> postorder = new List<int>();
+            Stack<TreeNode> treeStack = new Stack<TreeNode>();
+            HashSet<TreeNode> treeSet = new HashSet<TreeNode>();
+            if (root is null)
+            {
+                return postorder;
+            }
+
+            //put root on stack
+            treeStack.Push(root);
+            if (root.right is not null)
+            {
+                treeStack.Push(root.right);
+            }
+            if (root.left is not null)
+            {
+                treeStack.Push(root.left);
+            }
+
+            TreeNode current = root.left;
+
+            while (treeStack.Count > 0)
+            {
+                while (current is not null)
+                {
+                    if (current.right is not null && !treeSet.Contains(current.right))
+                    {
+                        treeStack.Push(current.right);
+                    }
+
+                    if (current.left is not null && !treeSet.Contains(current.left))
+                    {
+                        treeStack.Push(current.left);
+                        current = current.left;
+                    }
+                    else
+                    {
+                        if (current.right is not null && !treeSet.Contains(current.right))
+                        {
+                            current = current.right;
+                        }
+                        else
+                        {
+                            current = null!;
+                        }
+
+                    }
+                }
+                //current.left is null
+                //read value from top of stack
+                postorder.Add(treeStack.Peek().val);
+                //pop tree node at top of stack and add to visited
+                treeSet.Add(treeStack.Pop());
+                //set current
+                if (treeStack.Count > 0)
+                {
+                    current = treeStack.Peek();
+                }
+            }
+
+            return postorder;
+        }
+
+        public static IList<int> PostorderTraversalRecursion(TreeNode root)
+        {
+            List<int> postorder = new List<int>();
+            if (root is null)
+            {
+                return postorder;
+            }
+
+            PostorderRecursive(postorder, root);
+
+            return postorder;
+        }
+
+        private static void PostorderRecursive(List<int> postorder, TreeNode root)
+        {
+            if (root is null)
+            {
+                return;
+            }
+
+            //Recurse left
+            PostorderRecursive(postorder, root.left);
+
+            //Recurse right
+            PostorderRecursive(postorder, root.right);
+
+            //Add to list
+            postorder.Add(root.val);
+        }
+
+        public static IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            List<IList<int>> values = new List<IList<int>>();
+            if (root is null)
+            {
+                return values;
+            }
+
+            int level = 0;
+            List<IList<TreeNode>> nodes = new List<IList<TreeNode>>();
+
+            nodes.Add(new List<TreeNode>());
+            nodes[0].Add(root);
+
+            values.Add(new List<int>());
+            values[0].Add(root.val);
+
+            level++;
+
+            bool hasChildren = false;
+            if (root.left is not null || root.right is not null)
+            {
+                hasChildren = true;
+            }
+
+            while (hasChildren)
+            {
+                values.Add(new List<int>());
+                nodes.Add(new List<TreeNode>());
+                hasChildren = false;
+
+                foreach (TreeNode node in nodes[level - 1])
+                {
+                    //Add left child
+                    if (node.left is not null)
+                    {
+                        //add to node and value
+                        nodes[level].Add(node.left);
+                        values[level].Add(node.left.val);
+                        if (node.left.left is not null || node.left.right is not null)
+                        {
+                            hasChildren = true;
+                        }
+                    }
+                    //Add right child
+                    if (node.right is not null)
+                    {
+                        //add to node and value
+                        nodes[level].Add(node.right);
+                        values[level].Add(node.right.val);
+                        if (node.right.left is not null || node.right.right is not null)
+                        {
+                            hasChildren = true;
+                        }
+                    }
+                }
+                level++;
+            }
+            return values;
+        }
+
+        public static IList<IList<int>> LevelOrder2(TreeNode root)
+        {
+            List<IList<int>> values = new List<IList<int>>();
+            if (root is null)
+            {
+                return values;
+            }
+
+            int parentCount = 1;
+            int childCount = 0;
+            Queue<TreeNode> treeQueue = new Queue<TreeNode>();
+            treeQueue.Enqueue(root);
+            TreeNode current;
+
+            while (treeQueue.Count > 0)
+            {
+                List<int> level = new List<int>();
+                for (int i = 0; i < parentCount; i++)
+                {
+                    current = treeQueue.Dequeue();
+                    if (current.left is not null)
+                    {
+                        treeQueue.Enqueue(current.left);
+                        childCount++;
+                    }
+
+                    if (current.right is not null)
+                    {
+                        treeQueue.Enqueue(current.right);
+                        childCount++;
+                    }
+                    level.Add(current.val);
+                }
+                values.Add(level);
+                parentCount = childCount;
+                childCount = 0;
+            }
+
+            return values;
+        }
+
+        public static int MaxDepth(TreeNode root)
+        {
+            //Bottom up approach
+            if (root is null)
+            {
+                return 0;
+            }
+
+            int leftDepth = MaxDepth(root.left);
+            int rightDepth = MaxDepth(root.right);
+
+            return Math.Max(leftDepth, rightDepth) + 1;
         }
     }
 }
--- LearnUnitTesting/TestLibrary/BinaryTreeTests/BinaryTreeTravesalTests.cs
+++ LearnUnitTesting/TestLibrary/BinaryTreeTests/BinaryTreeTravesalTests.cs
@@ -139,10 +139,310 @@ namespace TestLibrary.BinaryTreeTests
             root.left.right.right = new TreeNode(5);
 
             IList<int> expected = new List<int> { 3, 2, 4, 5, 1 };
+
             // Act
             IList<int> actual = BinaryTreeTraversal.InorderTraversalRecursion(root);
             // Assert
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void PostorderTraversalRecursion()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(6);
+            root.left = new TreeNode(2);
+            root.left.left = new TreeNode(1);
+            root.left.right = new TreeNode(4);
+            root.left.right.left = new TreeNode(3);
+            root.left.right.right = new TreeNode(5);
+
+            root.right = new TreeNode(7);
+            root.right.right = new TreeNode(9);
+            root.right.right.left = new TreeNode(8);
+
+            IList<int> expected = new List<int> { 1, 3, 5, 4, 2, 8, 9, 7, 6 };
+
+            // Act
+            IList<int> actual = BinaryTreeTraversal.PostorderTraversalRecursion(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LevelOrder_ShouldReturnALevelOrderSequenceOfBinaryTree()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(1);
+            IList<IList<int>> expected = new List<IList<int>>();
+            expected.Add(new List<int>());
+            expected[0].Add(1);
+
+            // Act
+            IList<IList<int>> actual = BinaryTreeTraversal.LevelOrder(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LevelOrder_ShouldReturnALevelOrderSequenceOfBinaryTree2()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+
+            IList<IList<int>> expected = new List<IList<int>>();
+            expected.Add(new List<int>());
+            expected[0].Add(1);
+            expected.Add(new List<int>());
+            expected[1].Add(2);
+
+            // Act
+            IList<IList<int>> actual = BinaryTreeTraversal.LevelOrder(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LevelOrder_ShouldReturnALevelOrderSequenceOfBinaryTree3()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(4);
+            root.right.right = new TreeNode(5);
+
+            IList<IList<int>> expected = new List<IList<int>>();
+            expected.Add(new List<int>());
+            expected[0].Add(1);
+
+            expected.Add(new List<int>());
+            expected[1].Add(2);
+            expected[1].Add(3);
+
+            expected.Add(new List<int>());
+            expected[2].Add(4);
+            expected[2].Add(5);
+
+            // Act
+            IList<IList<int>> actual = BinaryTreeTraversal.LevelOrder(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LevelOrder_ShouldReturnALevelOrderSequenceOfBinaryTree4()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(4);
+            root.right.right = new TreeNode(5);
+            root.left.left.left = new TreeNode(6);
+            root.left.left.right = new TreeNode(7);
+            root.right.right.left = new TreeNode(8);
+            root.right.right.right = new TreeNode(9);
+
+            IList<IList<int>> expected = new List<IList<int>>();
+            expected.Add(new List<int>());
+            expected[0].Add(1);
+
+            expected.Add(new List<int>());
+            expected[1].Add(2);
+            expected[1].Add(3);
+
+            expected.Add(new List<int>());
+            expected[2].Add(4);
+            expected[2].Add(5);
+
+            expected.Add(new List<int>());
+            expected[3].Add(6);
+            expected[3].Add(7);
+            expected[3].Add(8);
+            expected[3].Add(9);
+
+            // Act
+            IList<IList<int>> actual = BinaryTreeTraversal.LevelOrder(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LevelOrder2_ShouldReturnALevelOrderSequenceOfBinaryTree()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(1);
+            IList<IList<int>> expected = new List<IList<int>>();
+            expected.Add(new List<int>());
+            expected[0].Add(1);
+
+            // Act
+            IList<IList<int>> actual = BinaryTreeTraversal.LevelOrder2(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LevelOrder2_ShouldReturnALevelOrderSequenceOfBinaryTree2()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+
+            IList<IList<int>> expected = new List<IList<int>>();
+            expected.Add(new List<int>());
+            expected[0].Add(1);
+            expected.Add(new List<int>());
+            expected[1].Add(2);
+
+            // Act
+            IList<IList<int>> actual = BinaryTreeTraversal.LevelOrder2(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        
+
+        [Fact]
+        public void LevelOrder2_ShouldReturnALevelOrderSequenceOfBinaryTree3()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(4);
+            root.right.right = new TreeNode(5);
+            root.left.left.left = new TreeNode(6);
+            root.left.left.right = new TreeNode(7);
+            root.right.right.left = new TreeNode(8);
+            root.right.right.right = new TreeNode(9);
+
+            IList<IList<int>> expected = new List<IList<int>>();
+            expected.Add(new List<int>());
+            expected[0].Add(1);
+
+            expected.Add(new List<int>());
+            expected[1].Add(2);
+            expected[1].Add(3);
+
+            expected.Add(new List<int>());
+            expected[2].Add(4);
+            expected[2].Add(5);
+
+            expected.Add(new List<int>());
+            expected[3].Add(6);
+            expected[3].Add(7);
+            expected[3].Add(8);
+            expected[3].Add(9);
+
+            // Act
+            IList<IList<int>> actual = BinaryTreeTraversal.LevelOrder2(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LevelOrder2_ShouldReturnALevelOrderSequenceOfBinaryTree4()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(4);
+            root.right.right = new TreeNode(5);
+
+            IList<IList<int>> expected = new List<IList<int>>();
+            expected.Add(new List<int>());
+            expected[0].Add(1);
+
+            expected.Add(new List<int>());
+            expected[1].Add(2);
+            expected[1].Add(3);
+
+            expected.Add(new List<int>());
+            expected[2].Add(4);
+            expected[2].Add(5);
+
+            // Act
+            IList<IList<int>> actual = BinaryTreeTraversal.LevelOrder2(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MaxDepth_ShouldReturnDepthOfABinaryTree()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(1);
+            int expected = 1;
+            // Act
+            int actual = BinaryTreeTraversal.MaxDepth(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MaxDepth_ShouldReturnDepthOfABinaryTree2()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            int expected = 2;
+            // Act
+            int actual = BinaryTreeTraversal.MaxDepth(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MaxDepth_ShouldReturnDepthOfABinaryTree3()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(4);
+            root.right.right = new TreeNode(5);
+            int expected = 3;
+            // Act
+            int actual = BinaryTreeTraversal.MaxDepth(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        
+        [Fact]
+        public void MaxDepth_ShouldReturnDepthOfABinaryTree4()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(4);
+            root.left.right = new TreeNode(5);
+            root.right.left = new TreeNode(6);
+            root.right.right = new TreeNode(7);
+            root.right.right.left = new TreeNode(8);
+            int expected = 4;
+            // Act
+            int actual = BinaryTreeTraversal.MaxDepth(root);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }        
     }
 }
